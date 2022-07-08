c05f852bb2bb6b1b791099c8f8a9419fda1a866e
MarcosAndreBueno <www.bueno@yahoo.com>
2022-07-08 13:34:46 -0300

    Abrir imagem pela própria classe.

--- interface_janela/Sobre.py
+++ interface_janela/Sobre.py
@@ -1,13 +1,12 @@
 from tkinter import Tk, Toplevel, PhotoImage, Label
-
 from PIL import ImageTk, Image
 
 
 class Sobre:
     def tela_sobre(self, img, root):
-
-        # criar janela
         outraJanela = Toplevel()
+        outraJanela.geometry("600x600")
+        img = PhotoImage(file=r"arquivos/sobre.png")
         lbl = Label(outraJanela, image=img)
         lbl.pack()
 
